7be819df8fe585163b6ad9a3e310e5ce0b7d8b97
irinatiron <irinatirongiurgiu@gmail.com>
2025-07-17 23:36:15 +0200

    chore: remove default Vite title and favicon / style(FormCreate): minor UI adjustements (#5)

--- src/pages/CreateButterfly.jsx
+++ src/pages/CreateButterfly.jsx
@@ -6,7 +6,7 @@ const CreateButterfly = () => {
     <>
       <h3 id='create-page-title'>Añadir una nueva mariposa al catálogo.</h3>
       <p id='create-page-intro'>¿No encuentras la mariposa que buscas? Puedes contribuir completando el siguiente formulario con sus datos para incluirla en nuestro listado.</p>
-      <img src='../../public/create-butterfly.png' id='create-butterfly'></img>
+      <img src='../../src/assets/images/create-butterfly.png' id='create-butterfly'></img>
       <Form onSubmit={createButterfly} />
     </>
   );
