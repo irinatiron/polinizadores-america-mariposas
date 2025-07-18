da8fdb50ed13a394b160bbfa95f0197e4fa17982
irinatiron <irinatirongiurgiu@gmail.com>
2025-07-18 17:01:55 +0200

    feat(FormCreate): update layout with icon integration, react-icons library, and responsive improvements (#5)

--- src/components/FormCreate.jsx
+++ src/components/FormCreate.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'; // Importa hook useState que maneja el estado del formulario
 import './FormCreate.css';
+import { MdOutlineColorLens } from "react-icons/md";
+import { LuRuler } from "react-icons/lu";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LuHourglass } from "react-icons/lu";
+import { HiOutlineHome } from "react-icons/hi";
+import { LuFlower2 } from "react-icons/lu";
+
 
 const Form = ({ onSubmit }) => {
   const [formData, setFormData] = useState({ // Se crea un estado local llamado formData con los valores del formulario
@@ -44,7 +51,7 @@ const Form = ({ onSubmit }) => {
     });
   };
 
-  const butterflyFamilies = [
+  const butterflyFamilies = [ // Las familias que existen de mariposas para el select
     'Nymphalidae',
     'Papilionidae',
     'Pieridae',
@@ -52,7 +59,6 @@ const Form = ({ onSubmit }) => {
     'Hesperiidae',
     'Riodinidae',
   ];
-
 
   return ( // Renderizado del form
     <form onSubmit={handleSubmit}>
@@ -64,19 +70,8 @@ const Form = ({ onSubmit }) => {
         </div>
         <div className="form-group" id='order-group'>
           <label htmlFor="input-order">Orden *</label>
-          {/* <input type="text" id="input-order" name="order" value={formData.order} onChange={handleChange} placeholder='Ej: Lepidoptera' required /> */}
-
           <input type="text" id="input-order" name="order" value={formData.order} readOnly />
-
-
         </div>
-
-
-        {/* <div className="form-group" id='family-group'>
-          <label htmlFor="input-family">Familia *</label>
-          <input type="text" id="input-family" name="family" value={formData.family} onChange={handleChange} placeholder='Ej: Nymphalidae' required />
-        </div> */}
-
         <div className="form-group" id='family-group'>
           <label htmlFor="input-family">Familia *</label>
           <select id="input-family" name="family" value={formData.family} onChange={handleChange} required>
@@ -88,48 +83,51 @@ const Form = ({ onSubmit }) => {
             ))}
           </select>
         </div>
-
-
-
       </div>
       <div id='optional-fields'>
         <p id='p-optional'>Información adicional</p>
+          <div className="form-group" id='img-group'>
+            <label htmlFor="input-img">Imagen</label>
+            <input type="url" id="input-img" name="img" value={formData.img} onChange={handleChange} placeholder='https://url-de-la-imagen.jpg' />
+          </div>
+          {/* Campo para una futura subida de archivos (imagen) por parte del usuario, necesario un back-end */}
+          {/* <div className="form-group" id='img-group'>
+          <label htmlFor="input-img">Imagen</label>
+          <input type="file" id="input-img" name="img" accept="image/*" onChange={handleChange} />
+        </div> */}
+          <div className="form-group" id='origin-group'>
+            <label htmlFor="input-origin">Origen</label>
+            <textarea id="input-origin" name="origin" value={formData.origin} onChange={handleChange} placeholder='Ej: Originaria de América del Norte.' />
+          </div>
+          <div className="form-group" id='location-group'>
+            <label htmlFor="input-location">Localización</label>
+            <textarea id="input-location" name="location" value={formData.location} onChange={handleChange} placeholder='Ej: Se distribuye desde Canadá hasta el norte de Sudamérica.' />
+          </div>
         <div className="form-group">
-          <label htmlFor="input-color">Color</label>
+          <label htmlFor="input-color"><MdOutlineColorLens /> Color</label>
           <textarea id="input-color" name="color" value={formData.color} onChange={handleChange} placeholder='Ej: Alas de color naranja con líneas negras.' />
         </div>
         <div className="form-group">
-          <label htmlFor="input-size">Tamaño</label>
+          <label htmlFor="input-size"><LuRuler /> Tamaño</label>
           <textarea id="input-size" name="size" value={formData.size} onChange={handleChange} placeholder='Ej: Entre 8,9 y 10,2 cm.' />
         </div>
         <div className="form-group">
-          <label htmlFor="input-origin">Origen</label>
-          <textarea id="input-origin" name="origin" value={formData.origin} onChange={handleChange} placeholder='Ej: Originaria de América del Norte.' />
+          <label htmlFor="input-fenology"><IoCalendarOutline /> Fenología</label>
+          <textarea id="input-fenology" name="fenology" value={formData.fenology} onChange={handleChange} placeholder='Ej: Migración en otoño, reproducción en primavera y verano.' />
         </div>
         <div className="form-group">
-          <label htmlFor="input-location">Localización</label>
-          <textarea id="input-location" name="location" value={formData.location} onChange={handleChange} placeholder='Ej: Se distribuye desde Canadá hasta el norte de Sudamérica.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-habitat">Hábitat</label>
-          <textarea id="input-habitat" name="habitat" value={formData.habitat} onChange={handleChange} placeholder='Ej: Hábitat diverso abarcando desde bosques a campos de algodoncillo.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-plants">Plantas visitadas</label>
-          <textarea id="input-plants" name="plants" value={formData.plants} onChange={handleChange} placeholder='Ej: Plantas del género Asclepias.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-cycle">Ciclo de vida</label>
+          <label htmlFor="input-cycle"><LuHourglass /> Ciclo de vida</label>
           <textarea id="input-cycle" name="cycle" value={formData.cycle} onChange={handleChange} placeholder='Ej: Huevo, oruga, crisálida y adulto.' />
         </div>
         <div className="form-group">
-          <label htmlFor="input-fenology">Fenología</label>
-          <textarea id="input-fenology" name="fenology" value={formData.fenology} onChange={handleChange} placeholder='Ej: Migración en otoño, reproducción en primavera y verano.' />
+          <label htmlFor="input-habitat"><HiOutlineHome /> Hábitat</label>
+          <textarea id="input-habitat" name="habitat" value={formData.habitat} onChange={handleChange} placeholder='Ej: Hábitat diverso abarcando desde bosques a campos de algodoncillo.' />
         </div>
-        <div className="form-group" id='img-group'>
-          <label htmlFor="input-img">Imagen</label>
-          <input type="url" id="input-img" name="img" value={formData.img} onChange={handleChange} placeholder='https://url-de-la-imagen.jpg' />
+        <div className="form-group">
+          <label htmlFor="input-plants"><LuFlower2 /> Plantas visitadas</label>
+          <textarea id="input-plants" name="plants" value={formData.plants} onChange={handleChange} placeholder='Ej: Plantas del género Asclepias.' />
         </div>
+
       </div>
       <button type="submit">+ Añadir mariposa</button>
     </form>
