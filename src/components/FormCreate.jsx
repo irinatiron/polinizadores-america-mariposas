6a3ec0c6c794ee96322e685f5110b282dadb1415
irinatiron <irinatirongiurgiu@gmail.com>
2025-07-18 18:57:21 +0200

    feat(FormCreate): improve UX with optional fields button and layout enhancements (#5)

--- src/components/FormCreate.jsx
+++ src/components/FormCreate.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'; // Importa hook useState que maneja el estado del formulario
 import './FormCreate.css';
+import { IoImageOutline } from "react-icons/io5";
+import { HiOutlineGlobeAmericas } from "react-icons/hi2";
+import { TbMapPin2 } from "react-icons/tb";
 import { MdOutlineColorLens } from "react-icons/md";
 import { LuRuler } from "react-icons/lu";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LuHourglass } from "react-icons/lu";
 import { HiOutlineHome } from "react-icons/hi";
 import { LuFlower2 } from "react-icons/lu";
+import { FaPlus } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+
+
 
 
 const Form = ({ onSubmit }) => {
@@ -23,6 +30,9 @@ const Form = ({ onSubmit }) => {
     img: '',
     fenology: '',
   });
+
+  const [showOptional, setShowOptional] = useState(false); // Vamos a dividir los campos en required (nombre, orden y familia) y opcionales (el resto)
+
 
   const handleChange = (e) => { // Manejamos los cambios realizados por el usuario
     const { name, value } = e.target; // Recoge name y value del input modificado
@@ -63,17 +73,16 @@ const Form = ({ onSubmit }) => {
   return ( // Renderizado del form
     <form onSubmit={handleSubmit}>
       <div id='required-fields'>
-        <p id='p-required'><i>* Campos obligatorios</i></p>
         <div className="form-group" id='name-group'>
-          <label htmlFor="input-name">Nombre *</label>
-          <input type="text" id="input-name" name="name" value={formData.name} onChange={handleChange} placeholder='Ej: Mariposa monarca (Danaus plexippus) ' required />
+          <label htmlFor="input-name">Nombre</label>
+          <input type="text" id="input-name" name="name" value={formData.name} onChange={handleChange} placeholder='Ej: Mariposa monarca (Danaus plexippus) ' required autoFocus />
         </div>
         <div className="form-group" id='order-group'>
-          <label htmlFor="input-order">Orden *</label>
+          <label htmlFor="input-order">Orden</label>
           <input type="text" id="input-order" name="order" value={formData.order} readOnly />
         </div>
         <div className="form-group" id='family-group'>
-          <label htmlFor="input-family">Familia *</label>
+          <label htmlFor="input-family">Familia</label>
           <select id="input-family" name="family" value={formData.family} onChange={handleChange} required>
             <option value="">Selecciona la familia</option>
             {butterflyFamilies.map((family) => (
@@ -84,10 +93,17 @@ const Form = ({ onSubmit }) => {
           </select>
         </div>
       </div>
-      <div id='optional-fields'>
-        <p id='p-optional'>Información adicional</p>
+      {/* Damos la opción al usuario de añadir información adicional a o de guardar la mariposa añadida sólo con el nombre y la familia */}
+      {!showOptional && (<div className="form-buttons"> 
+        <button type="button" className="show-optional" onClick={() => setShowOptional(true)}><FaPlus /> Añadir información adicional</button>
+        <button type="submit" className="submit-button">Añadir mariposa <FaCheck /></button>
+      </div>
+      )}
+      {/* Si el usuario elige añadir información adicional desplegamos el resto de campos */}
+      {showOptional && (
+        <div id='optional-fields'>
           <div className="form-group" id='img-group'>
-            <label htmlFor="input-img">Imagen</label>
+            <label htmlFor="input-img"><IoImageOutline /> Imagen</label>
             <input type="url" id="input-img" name="img" value={formData.img} onChange={handleChange} placeholder='https://url-de-la-imagen.jpg' />
           </div>
           {/* Campo para una futura subida de archivos (imagen) por parte del usuario, necesario un back-end */}
@@ -96,40 +112,41 @@ const Form = ({ onSubmit }) => {
           <input type="file" id="input-img" name="img" accept="image/*" onChange={handleChange} />
         </div> */}
           <div className="form-group" id='origin-group'>
-            <label htmlFor="input-origin">Origen</label>
+            <label htmlFor="input-origin"><HiOutlineGlobeAmericas /> Origen</label>
             <textarea id="input-origin" name="origin" value={formData.origin} onChange={handleChange} placeholder='Ej: Originaria de América del Norte.' />
           </div>
           <div className="form-group" id='location-group'>
-            <label htmlFor="input-location">Localización</label>
+            <label htmlFor="input-location"><TbMapPin2 /> Localización</label>
             <textarea id="input-location" name="location" value={formData.location} onChange={handleChange} placeholder='Ej: Se distribuye desde Canadá hasta el norte de Sudamérica.' />
           </div>
-        <div className="form-group">
-          <label htmlFor="input-color"><MdOutlineColorLens /> Color</label>
-          <textarea id="input-color" name="color" value={formData.color} onChange={handleChange} placeholder='Ej: Alas de color naranja con líneas negras.' />
+          <div className="form-group">
+            <label htmlFor="input-color"><MdOutlineColorLens /> Color</label>
+            <textarea id="input-color" name="color" value={formData.color} onChange={handleChange} placeholder='Ej: Alas de color naranja con líneas negras.' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-size"><LuRuler /> Tamaño</label>
+            <textarea id="input-size" name="size" value={formData.size} onChange={handleChange} placeholder='Ej: Entre 8,9 y 10,2 cm.' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-fenology"><IoCalendarOutline /> Fenología</label>
+            <textarea id="input-fenology" name="fenology" value={formData.fenology} onChange={handleChange} placeholder='Ej: Migración en otoño, reproducción en primavera y verano.' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-cycle"><LuHourglass /> Ciclo de vida</label>
+            <textarea id="input-cycle" name="cycle" value={formData.cycle} onChange={handleChange} placeholder='Ej: Huevo, oruga, crisálida y adulto.' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-habitat"><HiOutlineHome /> Hábitat</label>
+            <textarea id="input-habitat" name="habitat" value={formData.habitat} onChange={handleChange} placeholder='Ej: Hábitat diverso abarcando desde bosques a campos de algodoncillo.' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-plants"><LuFlower2 /> Plantas visitadas</label>
+            <textarea id="input-plants" name="plants" value={formData.plants} onChange={handleChange} placeholder='Ej: Plantas del género Asclepias.' />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="input-size"><LuRuler /> Tamaño</label>
-          <textarea id="input-size" name="size" value={formData.size} onChange={handleChange} placeholder='Ej: Entre 8,9 y 10,2 cm.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-fenology"><IoCalendarOutline /> Fenología</label>
-          <textarea id="input-fenology" name="fenology" value={formData.fenology} onChange={handleChange} placeholder='Ej: Migración en otoño, reproducción en primavera y verano.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-cycle"><LuHourglass /> Ciclo de vida</label>
-          <textarea id="input-cycle" name="cycle" value={formData.cycle} onChange={handleChange} placeholder='Ej: Huevo, oruga, crisálida y adulto.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-habitat"><HiOutlineHome /> Hábitat</label>
-          <textarea id="input-habitat" name="habitat" value={formData.habitat} onChange={handleChange} placeholder='Ej: Hábitat diverso abarcando desde bosques a campos de algodoncillo.' />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-plants"><LuFlower2 /> Plantas visitadas</label>
-          <textarea id="input-plants" name="plants" value={formData.plants} onChange={handleChange} placeholder='Ej: Plantas del género Asclepias.' />
-        </div>
-
-      </div>
-      <button type="submit">+ Añadir mariposa</button>
+      )}
+      {/* Si el usuario decidió añadir más campos adicionales le mostramos el botón de submit justo al final del formulario */}
+      {showOptional && (<button type="submit" id='endpage-add-button'>Añadir mariposa <FaCheck /></button>)}
     </form>
   );
 };
