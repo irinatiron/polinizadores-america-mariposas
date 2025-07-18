bb0e97f7ef282e1e356e48b68f9015d41d86b560
guiss26 <guissella584@gmail.com>
2025-07-18 13:09:33 -0700

    add CardButterfly.jsx an structure and style in CardButterfly.css (#1)

--- src/components/CardsButterflies.jsx
+++ src/components/CardsButterflies.jsx
@@ -3,22 +3,24 @@ const CardsButterflies = () => {
     return (
         /**A lo mejor aquí envolverlo en un section */
         <div className="cards">
-            <article className="card">
-                <div class="corner-ribbon">
-                        <h4><span>Mariposa-prueba</span></h4>
-                    </div>
-                    <div class="corner-order">
-                        <p><span>Order:</span> Legideptira</p>
-                    </div>
-                    {/* <img src="mariposa.jpg" alt="img-mariposa"> */}
-                    <div class="card-content">
-                        <p><span>Fenología: </span>Marzo a octubre</p>
-                        <p><span>Ciclo vital: </span> 5 meses, de verano a primavera</p>
-                    </div>
-                    <div class="btns">
-                        <a class="btn btn-ver-detalle" href="card.html">Ver detalle</a>
-                        <a class="btn btn-update" href="#">Editar</a>
-                    </div>
+            <article className="cards-butterflies">
+                <div className="corner-ribbon">
+                    <h4><span>Mariposa-prueba</span></h4>
+                </div>
+                <div className="corner-order">
+                    <p><span>Order:</span> Legideptira</p>
+                </div>
+                <div className="img-card">
+                    {/* <img src="" alt="img-mariposa"/> */}
+                </div>
+                <div className="card-content">
+                    <p><span>Fenología: </span>Marzo a octubre</p>
+                    <p><span>Ciclo vital: </span> 5 meses, de verano a primavera</p>
+                </div>
+                <div className="btns">
+                    <a className="btn btn-ver-detalle" href="card.html">Ver detalle</a>
+                    <a className="btn btn-update" href="#">Editar</a>
+                </div>
             </article>
         </div>
     )
