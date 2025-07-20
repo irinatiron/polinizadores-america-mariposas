6dcbb0bb8cdf37ac6de5ef236eab20e172ffc6d1
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-07-20 21:06:56 +0200

    FEAT: update Layout.jsx with comments and improved internatl structure (#1)

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,11 @@
+{/**Default component */}
 import { Outlet } from "react-router-dom"; {/*React Router component that renders the current page content */ }
+
+{/*Custom components*/}
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+{/**Style */}
 import "./Layout.css";
 
 {/**Creates the Layout component using arrow function syntax */}
