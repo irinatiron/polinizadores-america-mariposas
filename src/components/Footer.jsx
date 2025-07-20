c6e2bb7e7f9a63ef83f66f816c871d173e5af881
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-07-20 20:03:53 +0200

    CODE: create function for Footer.jsx and style for Footer.css(#9)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+function Footer() {
+    return (
+        <footer className="footer">
+
+            <div className="footer-content" style="border: 2px dotted gold">
+                <p className="footer-logo">Mariposas de América</p>
+                <p className="footer-text">Proyecto desarrollado con pasión por la naturaleza y la conservación</p>
+                <a href="" class="footer-brand">Factoría F5</a>
+            </div>
+
+        </footer>
+    )
+}
+export default Footer;
+
+
+
