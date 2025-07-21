26a7155932549af6b21e5c3f7d4861600e3924e2
guiss26 <guissella584@gmail.com>
2025-07-21 14:05:50 -0700

    add get method for front butterflies cards in CardsButterflies.jsx and ButterfliesServices.jsx (#2)

--- src/components/CardsButterflies.jsx
+++ src/components/CardsButterflies.jsx
@@ -1,29 +1,84 @@
 import './CardsButterflies.css';
-const CardsButterflies = () => {
+import { useEffect, useState } from 'react';
+import { getAllButterflies } from '../services/ButterflyServices'
+
+const ButterfliesCards = () => {
+    const [butterflies, setButterflies] = useState([])//
+    const [loading, setLoading] = useState(true)//
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getAllButterflies()
+                setButterflies(data)
+            } catch (error) {
+                console.error('Error al cargar mariposas: ', error);
+            } finally {
+                setLoading(false)
+            }
+        };
+
+        fetchData()
+    }, [])
+
+    if (loading) return <p>Cargando mariposas...</p>
+
     return (
-        /**A lo mejor aquí envolverlo en un section */
         <div className="cards">
-            <article className="cards-butterflies">
+            {butterflies.map((butterfly) => (
+            <article key={butterfly.id} className="cards-butterflies">
                 <div className="corner-ribbon">
-                    <h4><span>Mariposa-prueba</span></h4>
+                    <h4><span>{butterfly.name}</span></h4>
                 </div>
                 <div className="corner-order">
-                    <p><span>Order:</span> Legideptira</p>
+                    <p className='order-butterfly'><span>Order:</span> {butterfly.order}</p>
                 </div>
                 <div className="img-card">
-                    {/* <img src="" alt="img-mariposa"/> */}
+                    <img className='img-butterfly' src={butterfly.img} alt={`Imagen de ${butterfly.name}`}/>
                 </div>
                 <div className="card-content">
-                    <p><span>Fenología: </span>Marzo a octubre</p>
-                    <p><span>Ciclo vital: </span> 5 meses, de verano a primavera</p>
+                    <p><span>Fenología: </span>{butterfly.fenology}</p>
+                    <p><span>Ciclo vital: </span> {butterfly.cycle}</p>
                 </div>
                 <div className="btns">
                     <a className="btn btn-ver-detalle" href="card.html">Ver detalle</a>
                     <a className="btn btn-update" href="#">Editar</a>
                 </div>
             </article>
+            ))}
+
         </div>
     )
 }
 
-export default CardsButterflies
+export default ButterfliesCards
+
+
+// const CardsButterflies = () => {
+//     return (
+//         /**A lo mejor aquí envolverlo en un section */
+//         <div className="cards">
+//             <article className="cards-butterflies">
+//                 <div className="corner-ribbon">
+//                     <h4><span>Mariposa-prueba</span></h4>
+//                 </div>
+//                 <div className="corner-order">
+//                     <p><span>Order:</span> Legideptira</p>
+//                 </div>
+//                 <div className="img-card">
+//                     {/* <img src="" alt="img-mariposa"/> */}
+//                 </div>
+//                 <div className="card-content">
+//                     <p><span>Fenología: </span>Marzo a octubre</p>
+//                     <p><span>Ciclo vital: </span> 5 meses, de verano a primavera</p>
+//                 </div>
+//                 <div className="btns">
+//                     <a className="btn btn-ver-detalle" href="card.html">Ver detalle</a>
+//                     <a className="btn btn-update" href="#">Editar</a>
+//                 </div>
+//             </article>
+//         </div>
+//     )
+// }
+
+// export default CardsButterflies
--- src/services/ButterflyServices.jsx
+++ src/services/ButterflyServices.jsx
@@ -3,8 +3,22 @@
 const URL_API = "http://localhost:3000/butterflies";
 
 // Método GET para el READ
-async function getAllButterflies() {
+export async function getAllButterflies() {
+  // const response = await fetch(URL_API, {
+  //   method: "GET",
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  // const butterflyData = await response.json()
+  // console.log(butterflyData)
+  // return butterflyData
 
+  const response = await fetch(URL_API)
+  if (!response.ok) {
+    throw new Error('Error al obtener las mariposas')
+  }
+  return response.json()
 }
 async function getOneButterfly(id) {
 
@@ -33,26 +47,26 @@ export async function createButterfly(newButterfly) {
 }
 // Método PUT para el UPDATE
 async function updatebutterfly(id, updatedbutterfly) {
-try {
-const response = await fetch(`http://localhost:3000/butterflies"`, {
-method: "PUT",
-headers: {
-"Content-Type": "application/json",
-},
-body: JSON.stringify(updatedbutterfly),
-});
+  try {
+    const response = await fetch(`http://localhost:3000/butterflies"`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedbutterfly),
+    });
 
-if (!response.ok) {
-throw new Error("No se pudo actualizar ");
-}
+    if (!response.ok) {
+      throw new Error("No se pudo actualizar ");
+    }
 
-const data = await response.json();
-console.log("Actualizado:", data);
-return data;
+    const data = await response.json();
+    console.log("Actualizado:", data);
+    return data;
 
-} catch (error) {
-console.error("Error al actualizar:", error.message);
-}
+  } catch (error) {
+    console.error("Error al actualizar:", error.message);
+  }
 }
 
 
