e935290ba3efaa4c01b4034ced226553cb2cacad
irinatiron <irinatirongiurgiu@gmail.com>
2025-07-22 14:09:48 +0200

    feat(CreateForm): replace native alerts with SweetAlert in CreateForm (#5)

--- src/components/CreateForm/CreateForm.jsx
+++ src/components/CreateForm/CreateForm.jsx
@@ -11,16 +11,19 @@ import { HiOutlineHome } from "react-icons/hi";
 import { FaPlus, FaCheck } from "react-icons/fa";
 
 
+import Swal from 'sweetalert2' // Importa sweetalert
+
+
 const Form = ({ onSubmit }) => {
   const [formData, setFormData] = useState(initialFormState);
   const [formErrors, setFormErrors] = useState({}); // Se crea estado formErrors para almacenar los mensajes de error y por defecto está vacío
 
   const [showOptional, setShowOptional] = useState(false); // Vamos a dividir los campos en required (nombre, orden y familia) y opcionales (el resto)
-// Inicialmente los campos opcionales están ocultos: false
+  // Inicialmente los campos opcionales están ocultos: false
 
   const handleChange = (e) => { // handleChange es la función que se ejecuta cuando cambia un campo, se extrae name y value
     const { name, value } = e.target;
-    const updatedFormData = { 
+    const updatedFormData = {
       ...formData, // Spread operator: clona la información de formData
       [name]: value, // Los campos que han cambiado con su valor
     };
@@ -34,10 +37,36 @@ const Form = ({ onSubmit }) => {
   const handleSubmit = async (e) => {
     e.preventDefault(); // Evita que el navegador recargue la página
     const errors = validateButterfly(formData); // Validación final del formulario antes de permitir que envíe los datos
-    if (Object.keys(errors).length > 0) { 
-      alert("Errores en el formulario:\n" + Object.values(errors).join('\n'));
+   
+    const fieldLabels = { // Constante con la traducción de los campos
+      name: 'Nombre',
+      order: 'Orden',
+      family: 'Familia',
+      color: 'Color',
+      size: 'Tamaño',
+      origin: 'Origen',
+      location: 'Localización',
+      habitat: 'Hábitat',
+      plants: 'Plantas visitadas',
+      cycle: 'Ciclo de vida',
+      img: 'Imagen',
+      fenology: 'Fenología'
+    };
+
+    if (Object.keys(errors).length > 0) {
+      const firstErrorField = Object.keys(errors)[0];
+      const firstErrorMessage = errors[firstErrorField];
+      const fieldLabel = fieldLabels[firstErrorField] || firstErrorField;
+      Swal.fire({
+        icon: 'error',
+        title: 'Formulario inválido',
+        html: `<p>Por favor verifique lo que ha escrito en el campo <strong> ${fieldLabel} </strong> antes de continuar.</p><p>${firstErrorMessage}</p>`,
+        confirmButtonText: 'Entendido',
+        customClass: {confirmButton: 'swal2-confirm-error'} 
+      });
       return;
     }
+
     await onSubmit(formData); // Si no hay errores
     // Si el formulario se ha enviado correctamente se restablece a su valor inicial y se vacían todos los campos
     setFormData(initialFormState);
