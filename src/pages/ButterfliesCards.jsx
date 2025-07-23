9bb84f903c63e5b0d6dd6da44f0ed31e4495c050
guiss26 <guissella584@gmail.com>
2025-07-23 13:00:59 -0700

    add AddButterflyButton.jsx in ButterfliesCards.jsx(#2)

--- src/pages/ButterfliesCards.jsx
+++ src/pages/ButterfliesCards.jsx
@@ -1,10 +1,14 @@
 import CardFrontButterflies from '../components/CardFrontButterflies';
-
+// import { Link } from 'react-router-dom'
+import AddButterflyBtn from '../components/cards/AddButterflyButton';
 
 const ButterfliesCards = () => {
   return (
     <>
     <h2>Fichas de las mariposas</h2>
+    {/* <Link>+ Añadir mariposa</Link> */}
+
+    <AddButterflyBtn></AddButterflyBtn>
     <CardFrontButterflies/>
     </>
   )
