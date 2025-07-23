b4c3d69a35cf6098ed36f689c43a9a868652fc1f
guiss26 <guissella584@gmail.com>
2025-07-23 14:56:51 -0700

    merge index.css changes(#2)

--- src/pages/ButterfliesCards.jsx
+++ src/pages/ButterfliesCards.jsx
@@ -1,12 +1,9 @@
 import CardFrontButterflies from '../components/CardFrontButterflies';
-// import { Link } from 'react-router-dom'
 import AddButterflyBtn from '../components/cards/AddButterflyButton';
 
 const ButterfliesCards = () => {
   return (
     <>
-      {/* <h2>Fichas de las mariposas</h2>
-    <AddButterflyBtn></AddButterflyBtn> */}
       <div className="content-btn">
         <h2>Fichas de las mariposas</h2>
         <AddButterflyBtn></AddButterflyBtn>
