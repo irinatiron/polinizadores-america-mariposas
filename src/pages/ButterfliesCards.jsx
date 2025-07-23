d57a7af82a335fb19fedbcf0a62ee6fdc247f564
guiss26 <guissella584@gmail.com>
2025-07-23 14:00:52 -0700

    center h2 fichas mariposas and AddButterflyButton(#2)

--- src/pages/ButterfliesCards.jsx
+++ src/pages/ButterfliesCards.jsx
@@ -5,11 +5,14 @@ import AddButterflyBtn from '../components/cards/AddButterflyButton';
 const ButterfliesCards = () => {
   return (
     <>
-    <h2>Fichas de las mariposas</h2>
-    {/* <Link>+ Añadir mariposa</Link> */}
+      {/* <h2>Fichas de las mariposas</h2>
+    <AddButterflyBtn></AddButterflyBtn> */}
+      <div className="content-btn">
+        <h2>Fichas de las mariposas</h2>
+        <AddButterflyBtn></AddButterflyBtn>
+      </div>
 
-    <AddButterflyBtn></AddButterflyBtn>
-    <CardFrontButterflies/>
+      <CardFrontButterflies />
     </>
   )
 }
