1618aacc28e0a14751a072ad9bde62b1716de7f1
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-07-28 14:23:32 +0200

    FEAT: add 3 markers in leaflet map eeuu, mexico and brasil (#25)

--- src/components/AmericanMapLeaflet.jsx
+++ src/components/AmericanMapLeaflet.jsx
@@ -3,7 +3,22 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const AmericanMapLeaflet = () => {
-    const position = [51.505, -0.09]
+    const markers = [
+        {
+            position: [38.9072, -77.0369], // Washington D.C., EE.UU.
+            text: 'Estados Unidos: Washington D.C.',
+        },
+        {
+            position: [19.4326, -99.1332], // Ciudad de México
+            text: 'México: Ciudad de México',
+        },
+        {
+            position: [-15.7939, -47.8828], // Brasília, Brasil
+            text: 'Brasil: Brasília',
+        },
+    ];
+
+
     return (
 
         <MapContainer
@@ -16,11 +31,11 @@ const AmericanMapLeaflet = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {markers.map((marker, index) => (
+                <Marker key={index} position={marker.position}>
+                    <Popup>{marker.text}</Popup>
+                </Marker>
+            ))}
         </MapContainer>
 
     );
