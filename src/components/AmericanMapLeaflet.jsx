2b5b9741a0ace8658eb7237929def0cf39fbcffb
CarmenTajuelo <mcarmentajuelo@gmail.com>
2025-07-28 14:03:02 +0200

    FEAT: interactive leaflet map with 1 marker in London (#25)

--- src/components/AmericanMapLeaflet.jsx
+++ src/components/AmericanMapLeaflet.jsx
@@ -1,21 +1,29 @@
 // src/components/AmericanMapLeaflet.jsx
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const AmericanMapLeaflet = () => {
-  return (
-    <MapContainer
-      center={[37.0902, -95.7129]} // Coordenadas centradas en Estados Unidos
-      zoom={4}
-      scrollWheelZoom={false}
-      style={{ height: '100vh', width: '100%' }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
-  );
+    const position = [51.505, -0.09]
+    return (
+
+        <MapContainer
+            center={[37.0902, -95.7129]} // Coordenadas centradas en Estados Unidos
+            zoom={4}
+            scrollWheelZoom={false}
+            style={{ height: '100vh', width: '100%' }}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+
+    );
 };
 
 export default AmericanMapLeaflet;
